72f467f26962b18a18e45235f6a2bd9c6e491583
tananmurilo <tananmurilo@gmail.com>
2015-03-20 10:27:09 -0300

    Adição da classe Palavra reservada
    classe e metodo para verificar se uma palavra esta na lista de
    reservadas

--- Compiladores/src/Main.java
+++ Compiladores/src/Main.java
@@ -8,13 +8,22 @@
  *
  * @author murilo
  */
+
+
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("teste");
+        PalavrasReservadas reservadas = new PalavrasReservadas();
+        
+        System.out.println(reservadas.buscarPalavra("registro"));
     }
+    
     
 }
---
+++ Compiladores/src/PalavrasReservadas.java
@@ -0,0 +1,56 @@
+
+import java.util.LinkedList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author murilo
+ */
+public class PalavrasReservadas {
+    /**
+     * Inicializar a lista de palavras resevadas
+     * 
+     */
+    PalavrasReservadas(){
+      lista = new LinkedList<String>();
+        lista.add("algoritmo");
+        lista.add("variaveis");
+        lista.add("constantes");
+        lista.add("registro");
+        lista.add("funcao");
+        lista.add("retorno");
+        lista.add("vazio");
+        lista.add("se");
+        lista.add("senao");
+        lista.add("enquanto");
+        lista.add("para");
+        lista.add("leia");
+        lista.add("escreva");
+        lista.add("inteiro");
+        lista.add("real");
+        lista.add("booleano");
+        lista.add("char");
+        lista.add("cadeia");
+        lista.add("verdadeiro");
+        lista.add("falso");
+    }
+    
+    //declaração de variaveis, listas e etc
+    private LinkedList<String> lista;
+    
+    /**
+     * Metodo para verificar se a palavra buscada esta na lista de palavras resevadas.
+     * @param palavra string com a palavra procurada.
+     * @return true se for uma palavra reservada, false se não for.
+     */
+    public boolean buscarPalavra(String palavra){
+        return lista.contains(palavra);
+    }
+    
+    
+}
