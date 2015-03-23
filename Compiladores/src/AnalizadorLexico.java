5a5ce302dc26ace2c08925a09bf26db7c6412c1f
tananmurilo <tananmurilo@gmail.com>
2015-03-23 16:20:56 -0300

    Separação das linha em tokens
    No metodo separarTokens criei um modo de separar cada pedaço de texto ao
    encontrar um delimitador, espaço ou quebra de linha.

--- Compiladores/src/AnalizadorLexico.java
+++ Compiladores/src/AnalizadorLexico.java
@@ -27,8 +27,63 @@ public class AnalizadorLexico {
     
     };
     
-    private LinkedList<String> separarTokens(LinkedList<String> linhas){
+    /**
+     * Metodo vare cada linha e vai separando os pedaços ao encontrar um delimitador, espaço ou quebra de linha
+     * Ex: Entrada:int x, y;  ; 34. Saída: String[0]= 'int', String[1]= 'x', String[2]= ',',String[3]= 'y',String[4]= ';',String[5]= ';',String[6]= '34.',
+     * @param linhas lista com as linha do arquivo
+     * @return Um uma lista com cada sub string separada por um delimitador.
+     */
+    public LinkedList<String> separarTokens(LinkedList<String> linhas){
         LinkedList<String> expressoes = new LinkedList<String>();
+        
+        /**vare todas as linhas e vai separando os tokens quando enconta um delimitador.
+         * Idéia: a gente pode colocar para chamar o metodo do autonomo para analizar o token ao final de cada 
+         * separação feita e sair com uma lista de token já devidamente identificado, e caso o toke sejá invalido
+         * da pra pegar o numero da linha q ele está pq vai ser o indice do for.
+         */
+        for(int i=0; i<linhas.size(); i++){
+          String l = linhas.get(i);
+          //System.out.println(" linha "+i);
+          String temp ="";
+          int cont =0;
+          
+          while(l!=null && cont < l.length()){
+              char a =  l.charAt(cont);
+              //System.out.println("cont: "+cont+" tmax:"+ l.length());
+              //System.out.println("Linha: "+i+" Caractere: "+a);
+              
+              //se a variavel não tiver vazia vai armazenado os caracteres na variavel
+              if(!temp.isEmpty()){
+                  
+                  //se encontrar um delimitador ou espaço, armazena o que tiver na variavel temp na lista de tokens e inicializa a temp com o delimitador
+                  if(a==' ' || a==';' || a==',' || a=='(' || a==')' || a=='{' || a=='}' || a=='[' || a==']' ){
+                      expressoes.add(temp); //adiciona o token na lista
+                      temp = ""; //limpa a variavel
+                      String b = String.valueOf(a);
+                      if(a!=' '){ //se o que tiver separando as strings for um delimitador ele entra para variavel temporaria para na proxima interação virar um token também, e o espaço é ignorado.
+                          temp=temp+b;// adiciona o delimitador; 
+                      } 
+                  }else{ //se o caractere n for um delimitador ou espaço vai armazenando na string 
+                       
+                      String b = String.valueOf(a);
+                      temp= temp+b;// concatenar com os caracteres;
+                  }
+                  if((cont+1) == l.length()){//indentifica quando for quebra de linha(uma string representa uma linha então se não houver mais caractere na string é uma quebra de linha)
+                      expressoes.add(temp); //adiciona o token na lista
+                      temp = ""; //limpa a variavel
+                  }
+                  
+              }else{//se a variavel tiver vazia armazena o caracter eceto espaço.
+                  if(a!=' '){
+                      String b = String.valueOf(a);
+                      temp= temp+b;// concatenar com os caracteres;
+                  }
+              }
+              cont++;
+          }  
+          
+          cont=0;//zerar contador para o proximo while
+        }
         /*Aqui serão separadas pelo espaço e pelos delimitadores cada esxpressao a ser traduzida em um token*/
         return expressoes;
     };
--- Compiladores/src/Main.java
+++ Compiladores/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.LinkedList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,11 +29,24 @@ public class Main {
         
         Interface a = new Interface();
         a.setVisible(true);
-        //Arquivo arq = new Arquivo();
-        //arq.read("D:\\Users\\murilo\\Desktop\\arquivo teste.txt");
-        //System.out.println("");
-       // System.out.println("Teste na lista");
-        //arq.print();
+        Arquivo arq = new Arquivo();
+        
+         //abrir um arquivo pra teste mudar para o path do seu arquivo
+        arq.read("D:\\Users\\murilo\\Desktop\\arquivo teste.txt"); //abrir um arquivo pra teste mudar para o path do seu arquivo
+        System.out.println("");
+        System.out.println("Teste na lista do arquivo");
+        arq.print();
+        
+        AnalizadorLexico ana = new AnalizadorLexico();
+        LinkedList<String> Tokens = new LinkedList<String>();
+        Tokens =   ana.separarTokens(arq.getLinhas());
+         
+        System.out.println(" ");
+        System.out.println("Tokens gerados pelo texto");
+        for(String t:Tokens){
+            System.out.println(t);
+                    
+        }
     }
     
     
